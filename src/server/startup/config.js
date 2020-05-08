6b9693c8083e128a932886cfbe1b4fdd6f0b6c0a
emastra <emiliano.mastra@gmail.com>
2020-05-08 11:57:04 +0200

    build configuration

--- src/server/startup/config.js
+++ src/server/startup/config.js
@@ -8,9 +8,9 @@ module.exports = function(app) {
     throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
   }
 
-  app.set('views', path.join(__dirname, '../views'));
-  app.set('view engine', 'ejs');
+//   app.set('views', path.join(__dirname, '../views'));
+//   app.set('view engine', 'ejs');
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, '../../../public')));
+  app.use(express.static(path.join(__dirname, '../../../dist')));
 }
